527787d1f11f0e2c55461e65bf67fc687e8810e8
Ankur-cell-coder <ankurbgs6243@gmail.com>
2023-05-22 22:32:43 +0530

    partial done

--- src/Components/Autooffer.js
+++ src/Components/Autooffer.js
@@ -1,9 +1,206 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import HorizontalScroll from "react-scroll-horizontal";
+import { Link } from "react-router-dom";
+
+const data = [
+  {
+    id: "#ABC123454",
+    range:"Loreum ipsum",
+    sector:"Loreum ipsum",
+    tire:"Loreum ipsum"
+  },
+  {
+    id: "#ABC123454",
+    range:"Loreum ipsum",
+    sector:"Loreum ipsum",
+    tire:"Loreum ipsum"
+  },
+  {
+    id: "#ABC123454",
+    range:"Loreum ipsum",
+    sector:"Loreum ipsum",
+    tire:"Loreum ipsum"
+  },
+  {
+    id: "#ABC123454",
+    range:"Loreum ipsum",
+    sector:"Loreum ipsum",
+    tire:"Loreum ipsum"
+  },
+  {
+    id: "#ABC123454",
+    range:"Loreum ipsum",
+    sector:"Loreum ipsum",
+    tire:"Loreum ipsum"
+  },
+  {
+    id: "#ABC123454",
+    range:"Loreum ipsum",
+    sector:"Loreum ipsum",
+    tire:"Loreum ipsum"
+  },
+  {
+    id: "#ABC123454",
+    range:"Loreum ipsum",
+    sector:"Loreum ipsum",
+    tire:"Loreum ipsum"
+  },
+  {
+    id: "#ABC123454",
+    range:"Loreum ipsum",
+    sector:"Loreum ipsum",
+    tire:"Loreum ipsum"
+  },
+  {
+    id: "#ABC123454",
+    range:"Loreum ipsum",
+    sector:"Loreum ipsum",
+    tire:"Loreum ipsum"
+  },
+  {
+    id: "#ABC123454",
+    range:"Loreum ipsum",
+    sector:"Loreum ipsum",
+    tire:"Loreum ipsum"
+  },
+  {
+    id: "#ABC123454",
+    range:"Loreum ipsum",
+    sector:"Loreum ipsum",
+    tire:"Loreum ipsum"
+  },
+  {
+    id: "#ABC123454",
+    range:"Loreum ipsum",
+    sector:"Loreum ipsum",
+    tire:"Loreum ipsum"
+  }
+
+];
 
 function Autooffer() {
   return (
-    <div>Autooffer</div>
-  )
+    <Company>
+      <div className="uppersection">
+        <div>Domain</div>
+        <div>Organisation ID</div>
+      </div>
+
+      <div className="inputsection">
+        <input
+          placeholder="  Enter role of the company"
+          className="valuesection"
+        ></input>
+        <input
+          placeholder="  Enter Order Volume"
+          className="valuesection"
+        ></input>
+      
+        <input
+          placeholder="       Search"
+          style={{
+            width: "300px",
+            height: "30px",
+            marginTop: "10px",
+            marginLeft: "400px",
+          }}
+        ></input>
+      </div>
+
+     
+        <table style={{width:'1550px',height:'450px'}}>
+          <tr>
+            <th>Organisation ID</th>
+            <th>Amount range of financing</th>
+            <th>Domain/Sector Related Financing</th>
+            <th>Deep tire Financing</th>
+          </tr>
+
+          {data.map((val, key) => {
+            return (
+              <tr key={key}>
+               
+                <td>{val.id}</td>
+                <td>{val.range}</td>
+                <td>{val.sector}</td>
+                <td>{val.tire}</td>
+              </tr>
+            );
+          })}
+        </table>
+
+      <div className="lowersection">
+        <div>Showing 1 to 12 enteries</div>
+        <div
+          style={{
+            display: "flex",
+            width: "200px",
+            justifyContent: "space-between",
+          }}
+        >
+          <div>Previous</div>
+          <div>Next</div>
+        </div>
+      </div>
+    </Company>
+  );
 }
 
-export default Autooffer
+export default Autooffer;
+
+const Company = styled.div`
+  width: 99.8%;
+  height: 580px;
+  display: flex;
+  flex-direction: column;
+
+  @media only screen and (min-width: 1800px) {
+    // border:2px solid red;
+    width: 98%;
+    height: 810px;
+    margin-left: 20px;
+  }
+
+  table {
+    border-top: 2px solid black;
+    border-bottom:2px solid black;
+    width: 99.8%;
+    height: 160%;
+    margin-top: 20px;
+  }
+  .lowersection {
+    margin-left: 10px;
+    width: 1500px;
+    display: flex;
+    justify-content: space-between;
+    margin-top: 10px;
+  }
+  th {
+    border-bottom: 1px solid gray;
+  }
+  td {
+    text-align: center;
+    border-bottom: 1px solid gray;
+  }
+
+  .uppersection {
+    width: 453px;
+    display: flex;
+    justify-content: space-between;
+    margin-left: 10px;
+  }
+  .inputsection {
+    margin-left: 10px;
+    display: flex;
+    width: 1400px;
+    justify-content: space-between;
+  }
+  .valuesection {
+    margin-top: 10px;
+    display: flex;
+    width: 190px;
+    height: 30px;
+    justify-content: center;
+  }
+`;
--- src/Components/Organizationdatanotsynced.js
+++ src/Components/Organizationdatanotsynced.js
@@ -1,9 +1,320 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import HorizontalScroll from "react-scroll-horizontal";
+import { Link } from "react-router-dom";
+
+const data = [
+  {
+    name: "Acme Corporation",
+    id: "ABC123454",
+    status: "Loreum ipsum",
+    class: "Loreum ipsum",
+    category: "Synced",
+    subcategory: "Synced",
+    date: "Loreum ipsum",
+  },
+  {
+    name: "Acme Corporation",
+    id: "ABC123454",
+    status: "Loreum ipsum",
+    class: "Loreum ipsum",
+    category: "Synced",
+    subcategory: "Synced",
+    date: "Loreum ipsum",
+  },
+  {
+    name: "Acme Corporation",
+    id: "ABC123454",
+    status: "Loreum ipsum",
+    class: "Loreum ipsum",
+    category: "Synced",
+    subcategory: "Synced",
+    date: "Loreum ipsum",
+  },
+  {
+    name: "Acme Corporation",
+    id: "ABC123454",
+    status: "Loreum ipsum",
+    class: "Loreum ipsum",
+    category: "Synced",
+    subcategory: "Synced",
+    date: "Loreum ipsum",
+  },
+  {
+    name: "Acme Corporation",
+    id: "ABC123454",
+    status: "Loreum ipsum",
+    class: "Loreum ipsum",
+    category: "Synced",
+    subcategory: "Synced",
+    date: "Loreum ipsum",
+  },
+  {
+    name: "Acme Corporation",
+    id: "ABC123454",
+    status: "Loreum ipsum",
+    class: "Loreum ipsum",
+    category: "Synced",
+    subcategory: "Synced",
+    date: "Loreum ipsum",
+  },
+  {
+    name: "Acme Corporation",
+    id: "ABC123454",
+    status: "Loreum ipsum",
+    class: "Loreum ipsum",
+    category: "Synced",
+    subcategory: "Synced",
+    date: "Loreum ipsum",
+  },
+  {
+    name: "Acme Corporation",
+    id: "ABC123454",
+    status: "Loreum ipsum",
+    class: "Loreum ipsum",
+    category: "Synced",
+    subcategory: "Synced",
+    date: "Loreum ipsum",
+  },
+  {
+    name: "Acme Corporation",
+    id: "ABC123454",
+    status: "Loreum ipsum",
+    class: "Loreum ipsum",
+    category: "Synced",
+    subcategory: "Synced",
+    date: "Loreum ipsum",
+  },
+  {
+    name: "Acme Corporation",
+    id: "ABC123454",
+    status: "Loreum ipsum",
+    class: "Loreum ipsum",
+    category: "Synced",
+    subcategory: "Synced",
+    date: "Loreum ipsum",
+  },
+  {
+    name: "Acme Corporation",
+    id: "ABC123454",
+    status: "Loreum ipsum",
+    class: "Loreum ipsum",
+    category: "Synced",
+    subcategory: "Synced",
+    date: "Loreum ipsum",
+  },
+  {
+    name: "Acme Corporation",
+    id: "ABC123454",
+    status: "Loreum ipsum",
+    class: "Loreum ipsum",
+    category: "Synced",
+    subcategory: "Synced",
+    date: "Loreum ipsum",
+  },
+];
 
 function Organizationdatanotsynced() {
   return (
-    <div>Organizationdatanotsynced</div>
-  )
+    <Company>
+      <div className="uppersection">
+        <div>Comapny Name</div>
+        <div>Data Type</div>
+        
+      </div>
+
+      <div className="inputsection">
+        <input
+          placeholder="  Enter company name"
+          className="valuesection"
+        ></input>
+        <input
+          placeholder="  Select data type"
+          className="valuesection"
+        ></input>
+      
+        <input
+          placeholder="       Search"
+          style={{
+            width: "300px",
+            height: "30px",
+            marginTop: "10px",
+            marginLeft: "400px",
+          }}
+        ></input>
+      </div>
+
+      <HorizontalScroll>
+        <table style={{width:'1550px',height:'450px'}}>
+          <tr>
+            <th>Company Name</th>
+            <th>Corporate ID Number</th>
+            <th>Company Category</th>
+            <th>Email Address</th>
+            <th>Invoice/Vouchers</th>
+            <th>MSME Data</th>
+            <th>Datagov</th>
+          </tr>
+
+          {data.map((val, key) => {
+            return (
+              <tr key={key}>
+               
+                <td>{val.name}</td>
+                
+                <td>{val.id}</td>
+                <td>{val.status}</td>
+                <td>{val.class}</td>
+                <td>{val.category}</td>
+                <td>{val.subcategory}</td>
+                <td>{val.date}</td>
+              </tr>
+            );
+          })}
+        </table>
+        <table style={{width:'1550px',height:'450px'}}>
+          <tr>
+            <th>Registered State</th>
+            <th>Authorized Cap</th>
+            <th>Paidup Capital</th>
+            <th>Industrial Class</th>
+            <th>Principal Business Activity as per cin</th>
+            <th>Registered Office Address</th>
+            <th>Registrar of Companies </th>
+          </tr>
+
+          {data.map((val, key) => {
+            return (
+              <tr key={key}>
+                <td>{val.name}</td>
+                <td>{val.id}</td>
+                <td>{val.status}</td>
+                <td>{val.class}</td>
+                <td>{val.category}</td>
+                <td>{val.subcategory}</td>
+                <td>{val.date}</td>
+              </tr>
+            );
+          })}
+        </table> 
+        <table style={{ width: "1550px",height:'450px' }}>
+          <tr>
+            <th>Email Address </th>
+            <th>Latest Year Annual Return </th>
+            <th>Latest Year Financial Statement</th>
+            <th>Roc code</th>
+            <th>Registration Number</th>
+            <th>Number of Members</th>
+            <th>Date of Incorporation</th>
+          </tr>
+
+          {data.map((val, key) => {
+            return (
+              <tr key={key}>
+                <td>{val.name}</td>
+                <td>{val.id}</td>
+                <td>{val.status}</td>
+                <td>{val.class}</td>
+                <td>{val.category}</td>
+                <td>{val.subcategory}</td>
+                <td>{val.date}</td>
+              </tr>
+            );
+          })}
+        </table>
+        
+        <table style={{ width: "900px",height:'450px' }}>
+          <tr>
+            <th>Address Other </th>
+            <th>Company Listed Status</th>
+            <th>active_compliance</th>
+            <th>Suspended at Stock Exchange</th>
+          </tr>
+
+          {data.map((val, key) => {
+            return (
+              <tr key={key}>
+                <td>{val.name}</td>
+                <td>{val.id}</td>
+                <td>{val.status}</td>
+                <td>{val.class}</td>
+              </tr>
+            );
+          })}
+        </table>
+      </HorizontalScroll>
+
+      <div className="lowersection">
+        <div>Showing 1 to 12 enteries</div>
+        <div
+          style={{
+            display: "flex",
+            width: "200px",
+            justifyContent: "space-between",
+          }}
+        >
+          <div>Previous</div>
+          <div>Next</div>
+        </div>
+      </div>
+    </Company>
+  );
 }
 
-export default Organizationdatanotsynced
+export default Organizationdatanotsynced;
+
+const Company = styled.div`
+  width: 99.8%;
+  height: 580px;
+  display: flex;
+  flex-direction: column;
+
+  @media only screen and (min-width: 1800px) {
+    // border:2px solid red;
+    width: 98%;
+    height: 810px;
+    margin-left: 20px;
+  }
+
+  table {
+    border-top: 2px solid black;
+    border-bottom:2px solid black;
+    width: 99.8%;
+    height: 160%;
+    margin-top: 20px;
+  }
+  .lowersection {
+    margin-left: 10px;
+    width: 1500px;
+    display: flex;
+    justify-content: space-between;
+    margin-top: 10px;
+  }
+  th {
+    border-bottom: 1px solid gray;
+  }
+  td {
+    text-align: center;
+    border-bottom: 1px solid gray;
+  }
+
+  .uppersection {
+    width: 420px;
+    display: flex;
+    justify-content: space-between;
+    margin-left: 10px;
+  }
+  .inputsection {
+    margin-left: 10px;
+    display: flex;
+    width: 1400px;
+    justify-content: space-between;
+  }
+  .valuesection {
+    margin-top: 10px;
+    display: flex;
+    width: 190px;
+    height: 30px;
+    justify-content: center;
+  }
+`;

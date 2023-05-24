7b8797ebac91201c0e66337dde6f8145fc672bd9
Ankur-cell-coder <ankurbgs6243@gmail.com>
2023-05-24 23:10:50 +0530

    bids and data status page completed

--- src/Components/Dataadapter.js
+++ src/Components/Dataadapter.js
@@ -1,9 +1,220 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const data = [
+  {
+    data: "Lorm ipsum",
+    status: "Lorem",
+    link: "link",
+    count: "Lorem",
+    no_files: "5",
+    additional: "Loreum ipsum",
+  },
+  {
+    data: "Lorm ipsum",
+    status: "Lorem",
+    link: "link",
+    count: "Lorem",
+    no_files: "5",
+    additional: "Loreum ipsum",
+  },
+  {
+    data: "Lorm ipsum",
+    status: "Lorem",
+    link: "link",
+    count: "Lorem",
+    no_files: "5",
+    additional: "Loreum ipsum",
+  },
+  {
+    data: "Lorm ipsum",
+    status: "Lorem",
+    link: "link",
+    count: "Lorem",
+    no_files: "5",
+    additional: "Loreum ipsum",
+  },
+  {
+    data: "Lorm ipsum",
+    status: "Lorem",
+    link: "link",
+    count: "Lorem",
+    no_files: "5",
+    additional: "Loreum ipsum",
+  },
+  {
+    data: "Lorm ipsum",
+    status: "Lorem",
+    link: "link",
+    count: "Lorem",
+    no_files: "5",
+    additional: "Loreum ipsum",
+  },
+  {
+    data: "Lorm ipsum",
+    status: "Lorem",
+    link: "link",
+    count: "Lorem",
+    no_files: "5",
+    additional: "Loreum ipsum",
+  },
+  {
+    data: "Lorm ipsum",
+    status: "Lorem",
+    link: "link",
+    count: "Lorem",
+    no_files: "5",
+    additional: "Loreum ipsum",
+  },
+  {
+    data: "Lorm ipsum",
+    status: "Lorem",
+    link: "link",
+    count: "Lorem",
+    no_files: "5",
+    additional: "Loreum ipsum",
+  },
+  {
+    data: "Lorm ipsum",
+    status: "Lorem",
+    link: "link",
+    count: "Lorem",
+    no_files: "5",
+    additional: "Loreum ipsum",
+  },
+  {
+    data: "Lorm ipsum",
+    status: "Lorem",
+    link: "link",
+    count: "Lorem",
+    no_files: "5",
+    additional: "Loreum ipsum",
+  },
+  {
+    data: "Lorm ipsum",
+    status: "Lorem",
+    link: "link",
+    count: "Lorem",
+    no_files: "5",
+    additional: "Loreum ipsum",
+  },
+];
 
 function Dataadapter() {
   return (
-    <div>Dataadapter</div>
-  )
+    <Data1>
+      <div>
+        <button className="button1">Organisation Data</button>
+        <button className="button2">Peoples Data</button>
+      </div>
+
+      <input
+        placeholder="       Search"
+        style={{
+          width: "300px",
+          height: "30px",
+          marginTop: "30px",
+          marginLeft: "100px",
+        }}
+      ></input>
+
+      <table style={{ width: "1550px", height: "450px" }}>
+        <tr>
+          <th>Data Source</th>
+          <th>Status</th>
+          <th>S3 Link</th>
+          <th>Record Count</th>
+          <th>No.of Files</th>
+          <th>Additional Details</th>
+        </tr>
+
+        {data.map((val, key) => {
+          return (
+            <tr key={key}>
+              <td>{val.data}</td>
+              <td>{val.status}</td>
+              <td>{val.link}</td>
+              <td>{val.count}</td>
+              <td>{val.no_files}</td>
+              <td>{val.additional}</td>
+            </tr>
+          );
+        })}
+      </table>
+      <div className="lowersection">
+        <div>Showing 1 to 12 enteries</div>
+        <div
+          style={{
+            display: "flex",
+            width: "200px",
+            justifyContent: "space-between",
+          }}
+        >
+          <div>Previous</div>
+          <div>Next</div>
+        </div>
+      </div>
+    </Data1>
+  );
 }
 
-export default Dataadapter
+export default Dataadapter;
+
+const Data1 = styled.div`
+  width: 99.8%;
+  height: 580px;
+  display: flex;
+  flex-direction: column;
+
+  @media only screen and (min-width: 1800px) {
+    // border:2px solid red;
+    width: 98%;
+    height: 810px;
+    margin-left: 20px;
+  }
+
+  table {
+    border-top: 2px solid black;
+    border-bottom: 2px solid black;
+    width: 95.8%;
+    height: 160%;
+    margin-top: 20px;
+  }
+  .lowersection {
+    margin-left: 10px;
+    width: 1500px;
+    display: flex;
+    justify-content: space-between;
+    margin-top: 10px;
+  }
+  th {
+    border-bottom: 1px solid gray;
+  }
+  td {
+    text-align: center;
+    border-bottom: 1px solid gray;
+  }
+
+  .inputsection {
+    margin-left: 10px;
+    display: flex;
+    width: 1400px;
+    justify-content: space-between;
+  }
+  .button1 {
+    width: 185px;
+    height: 35px;
+    margin-left: 30px;
+    background: orange;
+    border: 2px solid orange;
+    color: white;
+  }
+  .button2 {
+    width: 185px;
+    height: 35px;
+    margin-left: 30px;
+    background: white;
+    border: 2px solid white;
+  }
+`;
--- src/Components/Finalizedbid.js
+++ src/Components/Finalizedbid.js
@@ -1,9 +1,185 @@
-import React from 'react'
 
-function Finalizedbid() {
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from "react-router-dom";
+
+const data = [
+  {
+   name:'Loreum ipsum',amount:'5000',days:'10', currentday:'12',financier:'Loreum ipsum',buyer:'Loreum ipsum',status:'Loreum',select:'Select'
+  },
+  {
+    name:'Loreum ipsum',amount:'5000',days:'10', currentday:'12',financier:'Loreum ipsum',buyer:'Loreum ipsum',status:'Loreum',select:'Select'
+   },
+   {
+    name:'Loreum ipsum',amount:'5000',days:'10', currentday:'12',financier:'Loreum ipsum',buyer:'Loreum ipsum',status:'Loreum',select:'Select'
+   },
+   {
+    name:'Loreum ipsum',amount:'5000',days:'10', currentday:'12',financier:'Loreum ipsum',buyer:'Loreum ipsum',status:'Loreum',select:'Select'
+   },
+   {
+    name:'Loreum ipsum',amount:'5000',days:'10', currentday:'12',financier:'Loreum ipsum',buyer:'Loreum ipsum',status:'Loreum',select:'Select'
+   },
+   {
+    name:'Loreum ipsum',amount:'5000',days:'10', currentday:'12',financier:'Loreum ipsum',buyer:'Loreum ipsum',status:'Loreum',select:'Select'
+   },
+   {
+    name:'Loreum ipsum',amount:'5000',days:'10', currentday:'12',financier:'Loreum ipsum',buyer:'Loreum ipsum',status:'Loreum',select:'Select'
+   },
+   {
+    name:'Loreum ipsum',amount:'5000',days:'10', currentday:'12',financier:'Loreum ipsum',buyer:'Loreum ipsum',status:'Loreum',select:'Select'
+   },
+   {
+    name:'Loreum ipsum',amount:'5000',days:'10', currentday:'12',financier:'Loreum ipsum',buyer:'Loreum ipsum',status:'Loreum',select:'Select'
+   },
+   {
+    name:'Loreum ipsum',amount:'5000',days:'10', currentday:'12',financier:'Loreum ipsum',buyer:'Loreum ipsum',status:'Loreum',select:'Select'
+   },
+   {
+    name:'Loreum ipsum',amount:'5000',days:'10', currentday:'12',financier:'Loreum ipsum',buyer:'Loreum ipsum',status:'Loreum',select:'Select'
+   },
+   {
+    name:'Loreum ipsum',amount:'5000',days:'10', currentday:'12',financier:'Loreum ipsum',buyer:'Loreum ipsum',status:'Loreum',select:'Select'
+   },
+
+
+];
+
+function Finalizebid() {
   return (
-    <div>Finalizedbid</div>
+   <Active1>
+
+<div className="uppersection">
+        <div>Comapny Name</div>
+        <div>Data Type</div>
+        
+      </div>
+
+      <div className="inputsection">
+        <input
+          placeholder="  Enter company name"
+          className="valuesection"
+        ></input>
+        <input
+          placeholder="  Select data type"
+          className="valuesection"
+        ></input>
+      
+        <input
+          placeholder="       Search"
+          style={{
+            width: "300px",
+            height: "30px",
+            marginTop: "10px",
+            marginLeft: "400px",
+          }}
+        ></input>
+      </div>
+
+    
+        <table style={{width:'1550px',height:'450px'}}>
+          <tr>
+            <th>Seller Name</th>
+            <th>Amount</th>
+            <th>No.of Days</th>
+            <th>Current Day Number</th>
+            <th>Financier Details</th>
+            <th>Buyer details</th>
+            <th>Status</th>
+            <th>Select</th>
+          </tr>
+
+          {data.map((val, key) => {
+            return (
+              <tr key={key}>
+               
+                <td>{val.name}</td>
+                 <td>{val.amount}</td>
+                <td>{val.days}</td>
+                <td>{val.currentday}</td>
+                <td>{val.financier}</td>
+                <td>{val.buyer}</td>
+                <td>{val.status}</td>
+                <td>{val.select}</td>
+               
+              </tr>
+            );
+          })}
+        </table>
+      <div className="lowersection">
+        <div>Showing 1 to 12 enteries</div>
+        <div
+          style={{
+            display: "flex",
+            width: "200px",
+            justifyContent: "space-between",
+          }}
+        >
+          <div>Previous</div>
+          <div>Next</div>
+        </div>
+      </div>
+
+   </Active1>
   )
 }
 
-export default Finalizedbid
+export default Finalizebid
+
+const Active1=styled.div`
+
+width: 99.8%;
+height: 580px;
+display: flex;
+flex-direction: column;
+
+@media only screen and (min-width: 1800px) {
+  // border:2px solid red;
+  width: 98%;
+  height: 810px;
+  margin-left: 20px;
+}
+
+table {
+  border-top: 2px solid black;
+  border-bottom:2px solid black;
+  width: 99.8%;
+  height: 160%;
+  margin-top: 20px;
+}
+.lowersection {
+  margin-left: 10px;
+  width: 1500px;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 10px;
+}
+th {
+  border-bottom: 1px solid gray;
+}
+td {
+  text-align: center;
+  border-bottom: 1px solid gray;
+}
+
+.uppersection {
+  width: 420px;
+  display: flex;
+  justify-content: space-between;
+  margin-left: 10px;
+}
+.inputsection {
+  margin-left: 10px;
+  display: flex;
+  width: 1400px;
+  justify-content: space-between;
+}
+.valuesection {
+  margin-top: 10px;
+  display: flex;
+  width: 190px;
+  height: 30px;
+  justify-content: center;
+}
+ 
+
+`;
